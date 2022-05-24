ccc458524b83935ede8adb28a6675028c6af776b
WasteofSpaceYT <82965910+WasteofSpaceYT@users.noreply.github.com>
2022-05-24 16:49:38 -0400

    checkForUpdates

--- phone/startup.lua
+++ phone/startup.lua
@@ -1,3 +1,4 @@
 function checkForUpdates()
     print("checking for updates")
+    shell.run("wget https://raw.githubusercontent.com/WasteofSpaceYT/TantaCraft/main/version/phone.txt")
 end
---
+++ security/startup.lua
@@ -0,0 +1,10 @@
+function checkForUpdates()
+    print("checking for updates")
+    shell.run("wget https://raw.githubusercontent.com/WasteofSpaceYT/TantaCraft/main/version/security.txt")
+    local file = fs.open("security.txt", "r")
+    local version = file.readLines()
+    for(i in version) do
+        if(version[i] ~= "") then
+            shell.run("wget " + version[i])
+        end
+end
